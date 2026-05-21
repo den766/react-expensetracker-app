a3cd6c46162cd2e7f7779319783691f58de3b43b
den766 <asharafalika886@gmail.com>
2026-05-21 12:27:30 +0530

    feat: add category filter UI for expenses

--- src/expenselist.jsx
+++ src/expenselist.jsx
@@ -1,27 +1,50 @@
 import ExpenseCard from "./expensecard";
 
-function ExpenseList({expenses , onDeleteExpense , onEditExpense , editingId , onUpdateExpense , onEditCancel , error}) {
+function ExpenseList({
+  expenses,
+  onDeleteExpense,
+  onEditExpense,
+  editingId,
+  onUpdateExpense,
+  onEditCancel,
+  error,
+}) {
+  const categories = [
+    "All",
+    "Food",
+    "Tranport",
+    "Utilities",
+    "Entertainment",
+    "Other",
+  ];
   return (
-    <ul>
-      {expenses.map((expense) => {
-        return (
-          <ExpenseCard
-            key={expense.id}
-            id={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            category={expense.category}
-            date={expense.createdAt}
-            deleteExpense={onDeleteExpense}
-            editExpense={onEditExpense}
-            editId = {editingId}
-            updateExpense= {onUpdateExpense}
-            cancelEdit={onEditCancel}
-            error={error}
-          />
-        );
-      })}
-    </ul>
+    <div>
+      <div className="filter_expenses">
+        {categories.map((cat) => (
+          <button key={cat}>{cat}</button>
+        ))}
+      </div>
+      <ul>
+        {expenses.map((expense) => {
+          return (
+            <ExpenseCard
+              key={expense.id}
+              id={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              category={expense.category}
+              date={expense.createdAt}
+              deleteExpense={onDeleteExpense}
+              editExpense={onEditExpense}
+              editId={editingId}
+              updateExpense={onUpdateExpense}
+              cancelEdit={onEditCancel}
+              error={error}
+            />
+          );
+        })}
+      </ul>
+    </div>
   );
 }
 
