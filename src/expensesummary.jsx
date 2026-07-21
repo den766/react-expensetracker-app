1a3a483707d692103ba49179fce4b1a0cf7cc118
den766 <asharafalika886@gmail.com>
2026-07-21 21:06:31 +0530

    style(summary): improve monthly summary mobile layout

--- src/expensesummary.jsx
+++ src/expensesummary.jsx
@@ -1,12 +1,8 @@
 import { formatCurrency } from "./utils/formatCurrency";
-
 function ExpenseSummary({ expenseSummary }) {
-  console.log(expenseSummary);
-
   return (
     <div className="expense_summary">
       <h3>Total Expenses</h3>
-
       <p className="summary_amount"> {formatCurrency(expenseSummary)}</p>
     </div>
   );
