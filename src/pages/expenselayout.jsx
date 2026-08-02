b16e24585aadbf6aa4e2d8f9f66aefadd190a699
den766 <asharafalika886@gmail.com>
2026-08-02 15:46:53 +0530

    feat: enhance dashboard navigation with Lucide icons

--- src/pages/expenselayout.jsx
+++ src/pages/expenselayout.jsx
@@ -1,37 +1,46 @@
 import { NavLink, Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/footer";
+import {
+  LayoutDashboard,
+  CirclePlus,
+  ReceiptText,
+  ChartColumn,
+} from "lucide-react";
 
 function ExpesneLayoutDashboard() {
   return (
     <div>
       <Header />
-      
+
       <nav className="dashboard-nav">
-        
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
           to={"/expense-dashboard"}
           end
         >
+          <LayoutDashboard size={18} />
           Overview
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
           to={"create-expense"}
         >
+          <CirclePlus size={18} />
           Create Expense
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
           to={"expense-list"}
         >
+          <ReceiptText size={18} />
           Expense List
         </NavLink>
         <NavLink
           className={({ isActive }) => (isActive ? "active" : "")}
           to={"reports"}
         >
+          <ChartColumn size={18} />
           Reports
         </NavLink>
       </nav>
