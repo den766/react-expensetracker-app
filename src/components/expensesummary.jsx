bc4f92549cc4b7d59ec81e387f2a036e482a4a48
den766 <asharafalika886@gmail.com>
2026-08-02 16:10:39 +0530

    feat: improve expense summary card presentation

--- src/components/expensesummary.jsx
+++ src/components/expensesummary.jsx
@@ -1,9 +1,15 @@
 import { formatCurrency } from "../utils/formatCurrency";
+import { HandCoins } from "lucide-react";
 function ExpenseSummary({ expenseSummary }) {
   return (
     <div className="expense_summary">
-      <h3>Total Expenses</h3>
+      <h3>
+        <HandCoins size={20} />
+        <span>Total Expenses</span>
+      </h3>
       <p className="summary_amount"> {formatCurrency(expenseSummary)}</p>
+
+      <p className="summary_subtitle">Total amount you've recorded</p>
     </div>
   );
 }
